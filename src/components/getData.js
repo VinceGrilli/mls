23de897d559c9f54fe2e42e023ab07d7eb4feceb
Vince Grilli <vgrilli1@gmail.com>
2022-03-11 16:25:58 -0500

    mess with fetch

--- src/components/getData.js
+++ src/components/getData.js
@@ -1,4 +1,13 @@
-const GetData = fetch('http://sparkapi.com/v1/standardfields')
-  .then(response => response.json())
-  .then(data => console.log(data))
-export default GetData
+async function GetData(url = 'http://sparkapi.com/v1/standardfields', data={}){
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Authorization: "Bearer ere5inazu2mtk1fpzn08xqpfh",
+      Accept: 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+  console.log(response.json)
+  return response.json();
+}
+export default GetData;
